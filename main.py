0d7831d6e00f630cfead8edcad4bd4e547c7c885
jonathanlal <jonathanmlaliberte@gmail.com>
2018-06-11 22:58:23 +0100

    changed module for GET requests, created two functions, one for processingInput and the other for processingUrl. Some AsciiArt too. Need to do the test run thing/handle slow or non responsive and additional stuff.

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import sys
-import httplib2 # pip install httplib2
-
-#consume input from stdin
-userInput = input("Enter a url: ")
-contents = httplib2.Http(".cache")
-(resp_headers, content) = contents.request(userInput, "GET")
+import requests # pip install requests
+import json
+import asciiArt
 
 
-#output stdout
-print(contents);
+#after typing in all the urls, user can type 'done' to start processing the urls
+def proccessInput():
+    buffer = []
+    while True:
+        line = sys.stdin.readline().rstrip('\n')
+        if line == 'done':
+            for x in buffer:
+                proccessUrl(x)
+            break
+        else:
+            buffer.append(line)
+
+
+#take in a url and stdout the results
+def proccessUrl(url):
+    data = {}
+    data['Url'] = url
+    try:
+        r = requests.get(url)
+        data['Status_code'] = r.status_code
+        data['Content_length'] = r.headers['Content-Length']
+        data['Date'] = r.headers['Date']
+    except Exception as e:
+        data['Error'] = str(e)
+    print(json.dumps(data, indent=4, sort_keys=True))
+    #asciiArt.makeArt('Some Finish Message','blue','on_cyan','doom',None)
+
+asciiArt.makeArt('BBC Challenge','yellow','on_red','doom','Seperate each URL by a new line.\nType \'done\' on a new line when you are done!\n')
+proccessInput()
